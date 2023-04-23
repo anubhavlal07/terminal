086b1e29197d6b8b3b1a92c10532a6531d9a9b1c
Anubhav Lal <67056859+anubhavlal07@users.noreply.github.com>
2023-04-23 18:11:32 +0530

    initial commit

---
+++ js/command.js
@@ -0,0 +1,75 @@
+var linkedin = "https://www.linkedin.com/in/anubhavl/";
+var instagram = "https://www.instagram.com/_anu_bhav/";
+var github = "https://github.com/anubhavlal07/";
+var email = "mailto:anubhavlal.15@gmail.com";
+var into = document.getElementById("intro");
+
+whois = [
+  "<br>",
+  "I am a recent computer science graduate from Bangalore Central University,",
+  "with a strong foundation in software development, web development, and data analytics.",
+  "I have a Bachelor in Computer Application and have completed relevant coursework in",
+  "Object-Oriented Programming, Data Structures and Algorithms, Database Systems, and Software Engineering.",
+  "I have experience in programming languages such as Java, C++, and Python, and have a good understanding of",
+  "web development technologies such as HTML, CSS, JavaScript, and jQuery.",
+  "<br>",
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is we never know, but, we constantly find out.",
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/anubhavlal' +
+    "</a>",
+  'instagram      <a href="' +
+    instagram +
+    '" target="_blank">instagram/_anu_bhav' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="_blank">github/anubhavlal07' +
+    "</a>",
+  "<br>",
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Use only if you\'re admin',
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>",
+];
+
+help = [
+  "<br>",
+  "<b>Commands       Action</b>",
+  "<br>",
+  '<span class="command">whois</span>          Who is Anubhav?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         God mode,requires password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not spam me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">intro</span>          Display the help keyword',
+  '<span class="command">pwd</span>            Display the current working directory',
+  "<br>",
+];
+
+presentWorkingDirectory = ["<br>", "/home/anubhavlal", "<br>"];
+
+intro = [
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+];
